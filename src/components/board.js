f46dc735f20bf5697374c3414b152d969e11df5a
Haraho <1438eogh@gmail.com>
2021-08-21 22:52:11 +0900

    add:posX and posY

--- src/components/board.js
+++ src/components/board.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function board() {
-  let posX;
-  let posY;
+  let posX = 0;
+  let posY = 0;
+  for (let i = 0; i < 10; i++) {
+    posX += posX;
+    for (let j = 0; j < 10; j++) {
+      posY += posY;
+      console.log(posX, +"," + posY);
+    }
+  }
 }
 
 export default board;
